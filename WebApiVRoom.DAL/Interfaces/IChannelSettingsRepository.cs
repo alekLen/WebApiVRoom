23d4fb6d4f0541254efbf6a85a5e8cd1799578d6
Vlad Bilous <cv33fdg@gmail.com>
2024-08-08 00:23:32 +0300

    Реалізація класа ChannelSettingsRepository

---
+++ WebApiVRoom.DAL/Interfaces/IChannelSettingsRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApiVRoom.DAL.Entities;
+
+namespace WebApiVRoom.DAL.Interfaces
+{
+    public interface IChannelSettingsRepository
+    {
+        Task Add(ChannelSettings ch);
+        Task Update(ChannelSettings ch);
+        Task<ChannelSettings> GetById(int id);
+        Task<IEnumerable<ChannelSettings>> GetAll();
+        Task Delete(int id);
+        Task<IEnumerable<ChannelSettings>> FindByOwner(int ownerId);
+    }
+}
--- WebApiVRoom.DAL/Repositories/ChannelSettingsRepository.cs
+++ WebApiVRoom.DAL/Repositories/ChannelSettingsRepository.cs
@@ -1,23 +1,71 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApiVRoom.DAL.EF;
 using WebApiVRoom.DAL.Entities;
+using WebApiVRoom.DAL.Interfaces;
 
 namespace WebApiVRoom.DAL.Repositories
 {
-    public class ChannelSettingsRepository
+    public class ChannelSettingsRepository : IChannelSettingsRepository
     {
+        private VRoomContext db;
+        public ChannelSettingsRepository(VRoomContext context)
+        {
+            db = context;
+        }
         public async Task Add(ChannelSettings ch)
         {
+            if (ch == null)
+            {
+                throw new ArgumentNullException(nameof(ch));
+            }
 
-           
+            await db.ChannelSettings.AddAsync(ch);
+            await db.SaveChangesAsync();
         }
+
         public async Task Update(ChannelSettings ch)
         {
+            if (ch == null)
+            {
+                throw new ArgumentNullException(nameof(ch));
+            }
 
+            db.ChannelSettings.Update(ch);
+            await db.SaveChangesAsync();
+        }
 
+        public async Task<ChannelSettings> GetById(int id)
+        {
+            return await db.ChannelSettings.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<ChannelSettings>> GetAll()
+        {
+            return await db.ChannelSettings.ToListAsync();
+        }
+
+        public async Task Delete(int id)
+        {
+            var channelSettings = await db.ChannelSettings.FindAsync(id);
+            if (channelSettings == null)
+            {
+                throw new KeyNotFoundException($"ChannelSettings with ID {id} not found.");
+            }
+
+            db.ChannelSettings.Remove(channelSettings);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<ChannelSettings>> FindByOwner(int ownerId)
+        {
+            return await db.ChannelSettings
+                           .Where(cs => cs.Owner.Id == ownerId)
+                           .ToListAsync();
         }
     }
 }
