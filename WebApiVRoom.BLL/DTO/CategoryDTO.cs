a2af74be9b8e4a06fa5c14d15127bed02883f81a
Iskra Oleksandr <_Iskra_@DESKTOP-4SUHQ4F>
2024-08-14 13:33:36 +0300

    Реализация классов CountryDTO, LanguageDTO, CategoryDTO и PostDTO, SubscriptionDTO, TagDTO

---
+++ WebApiVRoom.BLL/DTO/CategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiVRoom.DAL.Entities;
+
+namespace WebApiVRoom.BLL.DTO
+{
+    public class CategoryDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<int> VideosId { get; set; } = new List<int>();
+    }
+}
---
+++ WebApiVRoom.BLL/DTO/PostDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiVRoom.DAL.Entities;
+
+namespace WebApiVRoom.BLL.DTO
+{
+    public class PostDTO
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public int ChannelSettingsId { get; set; }
+        public DateTime Date { get; set; }
+        public string? Photo { get; set; }
+        public int LikeCount { get; set; }
+        public int DislikeCount { get; set; }
+        public List<int> CommentPostsId { get; set; } = new List<int>();
+    }
+}
