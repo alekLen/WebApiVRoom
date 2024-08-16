214a8486d12b79409a279148724b4908bc35635e
Iskra Oleksandr <_Iskra_@DESKTOP-4SUHQ4F>
2024-08-16 14:44:01 +0300

    Реализация классов AnswerPostDTO, AnswerVideoDTO, CommentPostDTO, CommentVideoDTO, HistoryOfBrowsingDTO, NotificationDTO, PlayListDTO и VideoDTO

---
+++ WebApiVRoom.BLL/DTO/CommentPostDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiVRoom.DAL.Entities;
+
+namespace WebApiVRoom.BLL.DTO
+{
+    public class CommentPostDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int PostId { get; set; }
+        public string Comment { get; set; }
+        public DateTime Date { get; set; }
+        public int LikeCount { get; set; }
+        public int DislikeCount { get; set; }
+        public int? AnswerPostId { get; set; }
+        public bool IsPinned { get; set; }
+        public bool IsEdited { get; set; }
+    }
+}
