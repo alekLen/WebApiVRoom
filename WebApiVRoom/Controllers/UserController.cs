355b1a890b5f759a3b44eebf40e85f638c78489a
Elena <aleklenoch@gmail.com>
2024-08-28 11:26:52 +0300

    #class  AddUserRequest

--- WebApiVRoom/Controllers/UserController.cs
+++ WebApiVRoom/Controllers/UserController.cs
@@ -5,6 +5,9 @@ using Microsoft.EntityFrameworkCore;
 using WebApiVRoom.BLL.Interfaces;
 using WebApiVRoom.BLL.Services;
 using WebApiVRoom.DAL.Repositories;
+using Microsoft.AspNetCore;
+using Microsoft.OpenApi.Any;
+using WebApiVRoom.DAL.Entities;
 
 namespace WebApiVRoom.Controllers
 {
@@ -14,10 +17,14 @@ namespace WebApiVRoom.Controllers
     {
 
         private IUserService _userService;
+        //var WEBHOOK_SECRET = Configuration["Clerk:WebhookSecret
+
+       
 
         public UserController(IUserService userService)
         {
             _userService = userService;
+           
         }
         
 
@@ -49,30 +56,84 @@ namespace WebApiVRoom.Controllers
 
             return Ok(usernew);
         }
-        [HttpPost("add/{clerkId}")]
-        public async Task<ActionResult<UserDTO>> AddUser([FromRoute] string clerkId)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            UserDTO user = await _userService.AddUser(clerkId);
-
-            return Ok(user);
-        }
-
-        //[HttpPost("add")]
-        //public async Task<ActionResult<UserDTO>> AddUser([FromBody] AddUserRequest request)
+        //[HttpPost("add/{clerkId}")]
+        //public async Task<ActionResult<UserDTO>> AddUser([FromRoute] string clerkId)
         //{
         //    if (!ModelState.IsValid)
         //    {
         //        return BadRequest(ModelState);
         //    }
 
-        //    UserDTO user = await _userService.AddUser(request);
+        //    UserDTO user = await _userService.AddUser(clerkId);
 
         //    return Ok(user);
+        //}
+
+        [HttpPost("add")]
+        public async Task<ActionResult<UserDTO>> AddUser([FromBody] AddUserRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+             UserDTO user = await _userService.AddUser(request.data.id);
+
+            return Ok(user);
+           
+        }
+
+        //[HttpPost("add")]
+        //public async Task<IActionResult> HandleWebhook()
+        //{
+        //    var WEBHOOK_SECRET = Environment.GetEnvironmentVariable("WEBHOOK_SECRET");
+
+        //    if (string.IsNullOrEmpty(WEBHOOK_SECRET))
+        //    {
+        //        throw new Exception("Please add WEBHOOK_SECRET from Clerk Dashboard to your environment variables");
+        //    }
+
+        //    var headerPayload = Request.Headers;
+        //    var svix_id = headerPayload["svix-id"];
+        //    var svix_timestamp = headerPayload["svix-timestamp"];
+        //    var svix_signature = headerPayload["svix-signature"];
+
+        //    if (string.IsNullOrEmpty(svix_id) || string.IsNullOrEmpty(svix_timestamp) || string.IsNullOrEmpty(svix_signature))
+        //    {
+        //        return BadRequest("Error occurred -- no svix headers");
+        //    }
+
+        //    string body;
+        //    using (var reader = new StreamReader(Request.Body))
+        //    {
+        //        body = await reader.ReadToEndAsync();
+        //    }
+
+        //    var wh = new Webhook(WEBHOOK_SECRET);
+
+        //    try
+        //    {
+        //        var evt = wh.Verify(body, new Dictionary<string, string>
+        //{
+        //    { "svix-id", svix_id },
+        //    { "svix-timestamp", svix_timestamp },
+        //    { "svix-signature", svix_signature }
+        //});
+
+        //        if (evt.Type == "user.created")
+        //        {
+        //            var userId = evt.Data.Id;
+        //            // Здесь вы можете добавить логику для сохранения userId в вашу базу данных
+        //            Console.WriteLine($"New user created with ID: {userId}");
+        //        }
+
+        //        return Ok();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.Error.WriteLine($"Error verifying webhook: {ex.Message}");
+        //        return BadRequest("Error occurred");
+        //    }
         //}
 
         [HttpDelete("{id}")]
