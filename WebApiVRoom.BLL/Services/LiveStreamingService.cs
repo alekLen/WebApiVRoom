eb5f233155c1b5566a398b5f57c64ad5b8841730
Vlad Bilous <cv33fdg@gmail.com>
2024-10-28 22:17:07 +0200

    google cloud api

---
+++ WebApiVRoom.BLL/Services/LiveStreamingService.cs
@@ -0,0 +1,74 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiVRoom.BLL.Services
+{
+    public class LiveStreamingService
+    {
+        private readonly YouTubeService _youTubeService;
+
+        public LiveStreamingService(string apiKey)
+        {
+            _youTubeService = new YouTubeService(new BaseClientService.Initializer
+            {
+                ApiKey = apiKey,
+                ApplicationName = "Vroom"  
+            });
+        }
+
+        public async Task<string> CreateLiveBroadcast(string title, string description, DateTime startTime)
+        {
+            var broadcast = new LiveBroadcast
+            {
+                Snippet = new LiveBroadcastSnippet
+                {
+                    Title = title,
+                    Description = description,
+                    ScheduledStartTime = startTime
+                },
+                Status = new LiveBroadcastStatus
+                {
+                    PrivacyStatus = "public" 
+                },
+                Kind = "youtube#liveBroadcast"
+            };
+
+            var request = _youTubeService.LiveBroadcasts.Insert(broadcast, "snippet,status");
+            var response = await request.ExecuteAsync();
+            return response.Id; 
+        }
+
+        public async Task<string> CreateLiveStream(string title)
+        {
+            var liveStream = new LiveStream
+            {
+                Snippet = new LiveStreamSnippet { Title = title },
+                Cdn = new CdnSettings
+                {
+                    IngestionType = "rtmp",
+                    Resolution = "720p",
+                    FrameRate = "30fps"
+                },
+                Kind = "youtube#liveStream"
+            };
+
+            var request = _youTubeService.LiveStreams.Insert(liveStream, "snippet,cdn");
+            var response = await request.ExecuteAsync();
+            return response.Id; 
+        }
+
+        public async Task<string> BindBroadcastToStream(string broadcastId, string streamId)
+        {
+            var bindRequest = _youTubeService.LiveBroadcasts.Bind(broadcastId, "id,contentDetails");
+            bindRequest.StreamId = streamId;
+            var bindResponse = await bindRequest.ExecuteAsync();
+            return bindResponse.Id;
+        }
+    }
+}
---
+++ WebApiVRoom.DAL/Repositories/BroadcastRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApiVRoom.DAL.EF;
+using WebApiVRoom.DAL.Entities;
+using WebApiVRoom.DAL.Interfaces;
+
+namespace WebApiVRoom.DAL.Repositories
+{
+    public class BroadcastRepository : IBroadcastRepository
+    {
+        private readonly VRoomContext _context;
+
+        public BroadcastRepository(VRoomContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> AddBroadcastAsync(Broadcast broadcast)
+        {
+            _context.Broadcasts.Add(broadcast);
+            await _context.SaveChangesAsync();
+            return broadcast.Id;
+        }
+
+        public async Task<Broadcast> GetBroadcastByIdAsync(string broadcastId)
+        {
+            return await _context.Broadcasts
+                .FirstOrDefaultAsync(b => b.BroadcastId == broadcastId);
+        }
+
+        public async Task UpdateBroadcastStatusAsync(string broadcastId, string status)
+        {
+            var broadcast = await _context.Broadcasts
+                .FirstOrDefaultAsync(b => b.BroadcastId == broadcastId);
+
+            if (broadcast != null)
+            {
+                broadcast.Status = status;
+                _context.Broadcasts.Update(broadcast);
+                await _context.SaveChangesAsync();
+            }
+        }
+        public async Task DeleteBroadcastAsync(string broadcastId)
+        {
+            var broadcast = await _context.Broadcasts
+                .FirstOrDefaultAsync(b => b.BroadcastId == broadcastId);
+
+            if (broadcast != null)
+            {
+                _context.Broadcasts.Remove(broadcast);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
