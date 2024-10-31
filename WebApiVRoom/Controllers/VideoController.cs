f435f67c42829a5a4a8575cdee28c200d3c0810e
Elena <aleklenoch@gmail.com>
2024-10-31 21:47:26 +0200

    #отправка SignalR сообщения при добавлении голоса

--- WebApiVRoom/Controllers/VideoController.cs
+++ WebApiVRoom/Controllers/VideoController.cs
@@ -386,19 +386,7 @@ namespace WebApiVRoom.Controllers
             if (!ModelState.IsValid)
             {
 
-        [HttpGet("getchannelvideos/{channelid}")]
-        public async Task<ActionResult<List<VideoInfoDTO>>> GetChannelVideos([FromRoute] int channelid)
-        {
-            List<VideoDTO> videos = await _videoService.GetByChannelId(channelid);
-            List<VideoInfoDTO> v = new List<VideoInfoDTO>();
-            foreach (var video in videos)
-            {
-                ChannelSettingsDTO channelSettings = await _chService.GetChannelSettings(video.ChannelSettingsId);
-                VideoInfoDTO videoInfo = ConvertVideoToVideoInfo(video, channelSettings);
-                v.Add(videoInfo);
-            }
-            return Ok(v);
-        }
+       
                 return BadRequest(ModelState);
             }
             VideoDTO videoDto = await _videoService.GetVideoInfo(video_id);
@@ -500,6 +488,20 @@ namespace WebApiVRoom.Controllers
         public async Task<ActionResult<IEnumerable<VideoDTO>>> GetShortVideosByChannelId(int channelId)
         {
             return new ObjectResult(await _videoService.GetShortVideosByChannelId(channelId));
+        }
+
+        [HttpGet("getchannelvideos/{channelid}")]
+        public async Task<ActionResult<List<VideoInfoDTO>>> GetChannelVideos([FromRoute] int channelid)
+        {
+            List<VideoDTO> videos = await _videoService.GetByChannelId(channelid);
+            List<VideoInfoDTO> v = new List<VideoInfoDTO>();
+            foreach (var video in videos)
+            {
+                ChannelSettingsDTO channelSettings = await _chService.GetChannelSettings(video.ChannelSettingsId);
+                VideoInfoDTO videoInfo = ConvertVideoToVideoInfo(video, channelSettings);
+                v.Add(videoInfo);
+            }
+            return Ok(v);
         }
     }
 }
